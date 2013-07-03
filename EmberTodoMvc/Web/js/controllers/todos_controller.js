bbbae19f6b3551055fb96e208b711240a0ac8062
nihique <kovarik.martin@gmail.com>
2013-07-03 21:29:21 +0200

    isplaying the number of incomplete todos

--- EmberTodoMvc/Web/js/controllers/todos_controller.js
+++ EmberTodoMvc/Web/js/controllers/todos_controller.js
@@ -1,15 +1,21 @@
 Todos.TodosController = Ember.ArrayController.extend({
     createTodo: function() {
         var title = this.get('newTitle');
-        if (!title.trim()) { return; }
-
+        if (!title.trim()) return;
         var todo = Todos.Todo.createRecord({
             title: title,
             isCompleted: false
         });
-
         todo.save();
-
         this.set('newTitle', '');
-    }
+    },
+
+    remaining: function() {
+        return this.filterProperty('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+
+    inflection: function() {
+        var remaining = this.get('remaining');
+        return remaining === 1 ? 'item' : 'items';
+    }.property('remaining')
 });
