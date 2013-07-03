f57f1c7d0dd489310cbcffaa681fca4aedd01713
nihique <kovarik.martin@gmail.com>
2013-07-03 23:19:12 +0200

    turned on logging in ember

--- EmberTodoMvc/Web/js/application.js
+++ EmberTodoMvc/Web/js/application.js
@@ -1,1 +1,9 @@
-window.Todos = Ember.Application.create();
+window.Todos = Ember.Application.create({
+    LOG_TRANSITIONS: true,
+    LOG_VIEW_LOOKUPS: true,
+    LOG_ACTIVE_GENERATION: true
+});
+
+Ember.LOG_VERSION = true;
+Ember.LOG_BINDINGS = true;
+Ember.LOG_STACKTRACE_ON_DEPRECATION = true;
