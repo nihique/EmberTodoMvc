3418eea40e6a92f56bd26ccbfc65d56c226c45d6
nihique <kovarik.martin@gmail.com>
2013-07-05 19:00:27 +0200

    all/active/completed routes

--- EmberTodoMvc/Web/js/router.js
+++ EmberTodoMvc/Web/js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
     this.resource('todos', { path: '/' }, function () {
         this.route('active');
+        this.route('completed');
     });
 })
 
@@ -13,10 +14,23 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 Todos.TodosActiveRoute = Ember.Route.extend({
     model: function () {
         return Todos.Todo.filter(function (todo) {
-           return todo.get('isCompleted') !== true;
-        })
+           if (todo.get('isCompleted')) { return true; }
+        });
     },
-    renderTemplate: function(controller) {
+    renderTemplate: function (controller) {
         this.render('todos/index', { controller: controller });
     }
 });
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function () {
+        return Todos.Todo.filter(function (todo) {
+            if (todo.get('isCompleted')) { return true; }
+        });
+    },
+    renderTemplate: function () {
+        debugger;
+        this.render('todos/index', { controller: 'todos' });
+    }
+})
+
