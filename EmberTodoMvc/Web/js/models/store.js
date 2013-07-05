4d0d38b1832cff1e7e19a2530427c0458cf39314
nihique <kovarik.martin@gmail.com>
2013-07-05 22:21:40 +0200

    fixture adapter replaced with local storage adapter

--- EmberTodoMvc/Web/js/models/store.js
+++ EmberTodoMvc/Web/js/models/store.js
@@ -1,3 +1,8 @@
 Todos.Store = DS.Store.extend({
-    adapter: 'DS.FixtureAdapter'
+//    adapter: 'DS.FixtureAdapter'
+    adapter: 'Todos.LSAdapter'
+});
+
+Todos.LSAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
 });
